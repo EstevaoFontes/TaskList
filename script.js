8be4b9a1e21edad3325655d9c9e560b66c3b0f96
EstevaoFontes <paulo.estevao21@gmail.com>
2023-06-29 20:12:28 -0300

    UPDATE 2.1

--- script.js
+++ script.js
@@ -12,41 +12,80 @@ const imageToChange = document.getElementById("modoImg")
 
 const titulo = document.querySelector(".titulo")
 
-let buttonNewTask = document.querySelector(".textoNovaTarefa").addEventListener("keydown", (e) =>{
-    if (e.key === "Enter"){
+
+
+let buttonNewTask = document.querySelector(".textoNovaTarefa").addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
         newTaskButton.click()
     }
 });
 
+function saveModal(name) {
+    localStorage.setItem("user", JSON.stringify(name))
+};
+
+function getModal() {
+    const modal = JSON.parse(localStorage.getItem("user"));
+    return modal
+};
+
+function modal() {
+    const modal = document.querySelector(".modal");
+    const inputModal = document.querySelector(".inputModal");
+    const buttonModal = document.querySelector(".buttonModal");
+
+    const modalSeted = getModal();
+    console.log(modalSeted)
+
+    if (modalSeted === null) {
+        modal.style.display = "flex";
+        newTaskText.style.cursor = "not-allowed"
+        newTaskText.setAttribute("disabled", "")
+        newTaskButton.style.cursor = "not-allowed"
+        newTaskButton.setAttribute("disabled", "")
+
+        buttonModal.addEventListener("click", () => {
+            titulo.innerText = `Olá ${inputModal.value}`
+            saveModal(inputModal.value)
+            newTaskText.style.cursor = "pointer";
+            newTaskText.removeAttribute("disabled", "");
+            newTaskButton.style.cursor = "pointer";
+            newTaskButton.removeAttribute("disabled", "");
+            modal.style.display = "none";
+        })
+    } else {
+        modal.style.display = "none";
+        titulo.innerText = `Olá ${modalSeted}`
+    }
+}
+
 newTaskButton.addEventListener("click", newTask)
 
-lightMode.addEventListener("click", () =>{
-        if (modoAtual == "lua"){
-            modoAtual = "sol"
-            lightOn()
-        }else{
-            modoAtual = "lua"
-            lightOff()
-        }
-    })
+lightMode.addEventListener("click", () => {
+    if (modoAtual == "lua") {
+        modoAtual = "sol"
+        lightOn()
+    } else {
+        modoAtual = "lua"
+        lightOff()
+    }
+})
 
 
-function lightOn(){
+function lightOn() {
     imageToChange.src = `imagens/${modoAtual}.png`
-    imageToChange.classList.add(`animar${modoAtual}`)    
 }
 
-function lightOff(){
-    imageToChange.classList.add(`animar${modoAtual}`)
+function lightOff() {
     imageToChange.src = `imagens/${modoAtual}.png`
 }
 
 
-function randonId(){
+function randonId() {
     return Math.floor(Math.random() * 5000)
 }
 
-function newTask(){
+function newTask() {
     const allTask = getNotes();
 
     const task = {
@@ -55,26 +94,24 @@ function newTask(){
     };
 
     constructor(task.texto, task.id);
-    
+
     newTaskText.value = "";
-    
+
     allTask.push(task)
-    
+
     saveNotes(allTask);
-    
+
 };
 
 
-function constructor(inputText, id){
+function constructor(inputText, id) {
 
     const container = document.createElement("div");
     container.setAttribute('id', id);
-    container.classList.add("tarefas");
 
     const areaTexto = document.createElement("textarea");
-    areaTexto.classList.add("areaTexto");
-    areaTexto.setAttribute("disabled","");
-    areaTexto.setAttribute("maxlength","377");
+    areaTexto.setAttribute("disabled", "");
+    areaTexto.setAttribute("maxlength", "377");
     areaTexto.value = inputText;
     container.appendChild(areaTexto);
 
@@ -84,7 +121,7 @@ function constructor(inputText, id){
     container.appendChild(botaoExcluir);
 
     const imagemExcluir = document.createElement("img");
-    imagemExcluir.setAttribute("src","imagens/excluir.png");
+    imagemExcluir.setAttribute("src", "imagens/excluir.png");
     imagemExcluir.classList.add("imagem_excluir");
     botaoExcluir.appendChild(imagemExcluir);
 
@@ -94,32 +131,32 @@ function constructor(inputText, id){
     container.appendChild(botaoEditar);
 
     const imagemEditar = document.createElement("img");
-    imagemEditar.setAttribute("src","imagens/editar.png");
+    imagemEditar.setAttribute("src", "imagens/editar.png");
     imagemEditar.classList.add("imagem_editar");
     botaoEditar.appendChild(imagemEditar);
 
-    const buttonTrash = container.querySelector(".botao_excluir_tarefa").addEventListener("click",(e) =>{
+    const buttonTrash = container.querySelector(".botao_excluir_tarefa").addEventListener("click", (e) => {
         notesTrash(e)
     })
 
-    const buttonEdit = container.querySelector(".botao_editar_tarefa").addEventListener("click",(e) =>{
+    const buttonEdit = container.querySelector(".botao_editar_tarefa").addEventListener("click", (e) => {
         verifiNotesEdit(e)
     })
-    
+
     main.appendChild(container);
 
 };
 
-function saveNotes(listTask){
-    localStorage.setItem("task",JSON.stringify(listTask))
+function saveNotes(listTask) {
+    localStorage.setItem("task", JSON.stringify(listTask))
 };
 
-function getNotes(){
+function getNotes() {
     const notes = JSON.parse(localStorage.getItem("task") || "[]")
     return notes
 };
 
-function showNotes (){
+function showNotes() {
     getNotes().forEach(notes => {
         const showNotes = constructor(notes.texto, notes.id)
         return showNotes
@@ -127,7 +164,7 @@ function showNotes (){
 }
 
 
-function notesTrash(e){
+function notesTrash(e) {
     let idNote = e.currentTarget.id;
 
     let notes = getNotes();
@@ -142,23 +179,23 @@ function notesTrash(e){
 
 let listasParaEdicao = []
 
-function verifiNotesEdit(e){
+function verifiNotesEdit(e) {
 
     let idnote = e.currentTarget.id
-    
+
     let index = listasParaEdicao.indexOf(idnote)
 
-    if (index != -1){
+    if (index != -1) {
         listasParaEdicao.splice(index, 1)
         desabilitarEdicao(e)
-    }else{
+    } else {
         listasParaEdicao.push(idnote)
         habilitarEdicao(e)
     }
 }
-    
-    
-function habilitarEdicao(e){
+
+
+function habilitarEdicao(e) {
 
     let idNote = e.currentTarget.id;
 
@@ -167,13 +204,13 @@ function habilitarEdicao(e){
     const imagemEditar = container.children[2].children[0];
 
     areaTexto.removeAttribute("disabled");
-    imagemEditar.setAttribute("src","imagens/confirmação.png");
+    imagemEditar.setAttribute("src", "imagens/confirmação.png");
     container.style.border = "1px solid green";
-}    
+}
 
 
 
-function desabilitarEdicao(e){
+function desabilitarEdicao(e) {
     const notes = getNotes();
     console.log(notes)
     let idNote = e.currentTarget.id;
@@ -183,19 +220,20 @@ function desabilitarEdicao(e){
     const imagemEditar = container.children[2].children[0];
 
     areaTexto.setAttribute("disabled", "");
-    imagemEditar.setAttribute("src","imagens/editar.png");
+    imagemEditar.setAttribute("src", "imagens/editar.png");
     container.style.border = "1px solid white";
 
-    for (let i=0; i<notes.length; i++){
-        if (notes[i].id == idNote){
-           notes[i].texto = areaTexto.value
-           saveNotes(notes)
+    for (let i = 0; i < notes.length; i++) {
+        if (notes[i].id == idNote) {
+            notes[i].texto = areaTexto.value
+            saveNotes(notes)
         }
     }
 }
 
 //INICIALIZAÇÃO
-showNotes ()
+showNotes()
+modal()
 
 
 
